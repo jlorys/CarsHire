70d78646017ab47d290a9fd16e76ad932db876cd
Grzegorz <grzes77_18@o2.pl>
2017-04-20 20:56:11 +0200

    Add class seller

---
+++ src/main/java/carshire/domain/Seller.java
@@ -0,0 +1,38 @@
+package carshire.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@Entity
+@NoArgsConstructor
+public class Seller {
+    
+    public enum Rights{
+        Admin,
+        Manager,
+        Employee
+    };
+        
+    @Id @GeneratedValue
+	private Long id;
+    private String firstName;
+    private String lastName;
+    private String login;
+	private String eMail;
+    private String password;
+    private String city;
+    private String street;
+    private String houseNumber;
+    @Column(name = "rights")
+    @Enumerated(EnumType.STRING)
+    private Rights rights;
+}
