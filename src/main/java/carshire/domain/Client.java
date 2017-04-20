8aca34099e9209900c849e618dad7b18fed310f9
Marcin <Marcin@Marcin-Komputer>
2017-04-20 16:12:17 +0200

    Add class Client

---
+++ src/main/java/carshire/domain/Client.java
@@ -0,0 +1,26 @@
+package carshire.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@Entity
+@NoArgsConstructor
+public class Client {
+    
+    @Id @GeneratedValue
+	private Long id;
+    private String firstName;
+    private String lastName;
+	private String eMail;
+    private String city;
+    private String street;
+    private String houseNumber;
+    private Integer discount;
+    
+}
